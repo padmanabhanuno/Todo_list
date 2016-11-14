dfedfedfccec65df00aeedd39a1f601c60a85e02
Padmanabhan Soundararajan <psoundararajan@unomaha.edu>
2016-11-13 23:49:12 -0600

    final draft

--- www/todo-localstorage.js
+++ www/todo-localstorage.js
@@ -1,93 +1,65 @@
 var todoStorage = {
-    collection:[]
+    collection: []
 };
 
-todoStorage.init=function(){
-    this.collection=JSON.parse(localStorage.getItem('ToDo')||'[]');
-
+todoStorage.init = function(){
+    this.collection= JSON.parse(localStorage.getItem('todo') || '[]');
 };
-todoStorage.hasItem=function(label){
+todoStorage.hasItem = function(label){
     return this.collection.some(function(item){
-        return item.label==label;
+        return item.label === label;
     });
 };
-todoStorage.save=function(){
-    localStorage.setItem('ToDo',JSON.stringify(this.collection));
-    
+todoStorage.save = function(){
+    localStorage.setItem('todo', JSON.stringify(this.collection));
 };
 
-todoStorage.add=function(label){
+todoStorage.add = function(label){
     if(this.hasItem(label)){
         return false;
-        
     }
     this.collection.push({
-        label:label,
-        status:'uncompleted'
+        label: label,
+        status: 'uncompleted'
+    });
+    this.save();
+    return true;
+};
+todoStorage.remove = function(label){
+     if(!this.hasItem(label)){
+        return false;
+    }
+    this.collection.forEach(function(item, i){
+        if(item.label === label) {
+            this.collection.splice(i,1);
+        }
+    }.bind(this));
+    this.save();
+    return true;
+};
+
+todoStorage.toggleStatus = function(label){
+    if(!this.hasItem(label)) {
+        return false;
+    }
+    
+    this.collection.forEach(function(item, i){
+        if(item.label === label) {
+            item.status = item.status === 'completed' ? 'uncompleted' : 'completed';
+        }
     });
     this.save();
     return true;
 };
 
-todoStorage.remove=function(label){
-    if(!this.hasItem(label)){
-        return false;
-        
-    }
-    this.collection.forEach(function(item,i){
-        if(item.label==label){
-            this.collection.splice(i,1);
-        
-        }
-    });
-    this.save()
-    return true;
-    
-};
-todoStorage.toggleStatus=function(label){
-    
-    if(!this.hasItem(label)){
-    return false;
-        
-    }
-    this.collection.forEach(function(item,i){
-        if(item.label==label){
-            item.status=item.status==='completed' ? 'uncompleted':'completed';
-        
-        }
-    });
-    this.save()
-    return true;
-    
-};
-
-todoStorage.filter=function(status){
-    if(status=='all'){
+todoStorage.filter = function(status){
+    if(status === 'all'){
         return this.collection;
-        
     }
     return this.collection.filter(function(item){
-        return item.status===status;
+        return item.status === status
     });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 
